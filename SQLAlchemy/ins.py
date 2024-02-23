8267bba5f3d11a0f4a7e96ac4e36e072bece4dcf
ashikagamasaaki <gamasaga0312@gmail.com>
2024-02-23 21:47:11 +0900

    DB設定モジュール修正

--- SQLAlchemy/ins.py
+++
@@ -1,9 +0,0 @@
-from dbengine import User
-from dbopen import SessionCreater
-
-test_user = User(user_id='10002', first_name='Hanako', last_name='Yamada', age=24)
-
-session = SessionCreater.makeSession()
-session.add(test_user)
-session.commit()
-session.close()
