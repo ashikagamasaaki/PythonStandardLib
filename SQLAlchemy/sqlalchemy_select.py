9dab718de765fcab68df0765218c520e8bacfa77
ashikagamasaaki <gamasaga0312@gmail.com>
2024-02-24 12:55:33 +0900

    SQLAlchemy SELECT処理(JOIN)

--- SQLAlchemy/sqlalchemy_select.py
+++ SQLAlchemy/sqlalchemy_select.py
@@ -1,4 +1,4 @@
-from db.dbengine import User
+from db.dbengine import User, Assign, Department, Employee
 from db.dbopen import SessionCreater
 
 def getSession():
@@ -22,11 +22,34 @@ def getFilterUser(user_id):
     return user
 
 
+def getEmpDept(isouter_flg=False):
+    session = getSession()
+    user_dept = session.query(Employee, Employee.emp_id, Employee.emp_name, Department.dept_id, Department.dept_name).\
+        join(Assign, Employee.emp_id == Assign.emp_id, isouter=isouter_flg).\
+        join(Department, Assign.dept_id == Department.dept_id, isouter=isouter_flg).\
+        all()
+    session.close()
+    return user_dept
+
+
+
+
 if __name__ == '__main__':
-    users = getAllUser()
-    for user in users:
-        print(f'User ... user_id: {user.user_id}, first_name: {user.first_name}, last_name: {user.last_name}')
+    # users = getAllUser()
+    # for user in users:
+    #     print(f'User ... user_id: {user.user_id}, first_name: {user.first_name}, last_name: {user.last_name}')
     
     
-    filter_user = getFilterUser(10003)
-    print(f'Filter User ... user_id: {user.user_id}, first_name: {user.first_name}, last_name: {user.last_name}')
+    # filter_users = getFilterUser(10003)
+    # for filter_user in filter_users:
+    #     print(f'Filter User ... user_id: {filter_user.user_id}, first_name: {filter_user.first_name}, last_name: {filter_user.last_name}')    
+    
+    
+    # emp_dept_datas = getEmpDept()
+    # for emp_dept_data in emp_dept_datas:
+        # print(f'Emp: {emp_dept_data.emp_name}({emp_dept_data.emp_id}), Dept: {emp_dept_data.dept_id}({emp_dept_data.dept_name})')
+    
+    
+    all_emp_dept_datas = getEmpDept(isouter_flg=True)
+    for emp_dept_data in all_emp_dept_datas:
+        print(f'Emp: {emp_dept_data.emp_name}({emp_dept_data.emp_id}), Dept: {emp_dept_data.dept_id}({emp_dept_data.dept_name})')
